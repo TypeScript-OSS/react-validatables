01ec1664d29b47155757f38b14acfea91a21b2c1
Brian Westphal <brian.westphal@panchaininc.com>
2022-07-19 08:31:14 +0800

    v1.0.0-alpha.4: - Fixed type declartion for ValidationError

--- src/validator/types/validation-error.ts
+++ src/validator/types/validation-error.ts
@@ -1,4 +1,4 @@
 import type { TFunction } from 'i18next';
 import type { TypeOrDeferredTypeWithArgs } from 'react-bindings';
 
-export type ValidationError = TypeOrDeferredTypeWithArgs<string, [] | [TFunction | undefined]>;
+export type ValidationError = TypeOrDeferredTypeWithArgs<string, [TFunction | void]>;
